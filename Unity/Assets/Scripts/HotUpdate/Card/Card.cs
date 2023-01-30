2029f2d94daba3666c3b0033d54c8b819c1e06ef
PV-11\Admin <753631776@qq.com>
2023-01-31 00:09:51 +0800

    add

---
+++ Unity/Assets/Scripts/HotUpdate/Card/Card.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ThisGame {
+    /// <summary>
+    /// ¿¨Æ¬
+    /// </summary>
+    public class Card : MonoBehaviour {
+        
+    }
+}
